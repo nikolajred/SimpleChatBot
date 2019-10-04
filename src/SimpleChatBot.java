6d6108367877149dd7804faa952f84f35d5500d9
nikolajred <nikolajred@gmail.com>
2019-10-04 13:28:21 +0300

    first commit

--- src/SimpleChatBot.java
+++ src/SimpleChatBot.java
@@ -1,17 +1,19 @@
 import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyledDocument;
 import java.awt.*;
 import java.awt.event.*;
 
-public class SimpleChatBot extends JFrame {
+public class SimpleChatBot extends JFrame implements ActionListener{
     final String TITLE_OF_PROGRAMM = "Chatter: simple chatbot";
     final int START_LOCATION = 200;
     final int WINDOW_WIDTH = 350;
     final int WINDOW_HIGHT = 450;
-    JTextArea dialoge;
+    JTextArea dialogue;
     JCheckBox ai;
     JTextField message;
 
-    //SimpleBot sbot;
+    SimpleBot sbot;
     public static void main(String[] args) {
         new SimpleChatBot();
     }
@@ -21,23 +23,35 @@ public class SimpleChatBot extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(START_LOCATION, START_LOCATION, WINDOW_WIDTH, WINDOW_HIGHT);
         setVisible(true);
-        dialoge = new JTextArea();
-        dialoge.setLineWrap(true);
-        JScrollPane scrollBar = new JScrollPane(dialoge);
+        dialogue = new JTextArea();
+        dialogue.setLineWrap(true);
+        JScrollPane scrollBar = new JScrollPane(dialogue);
         JPanel bp = new JPanel();
         bp.setLayout(new BoxLayout(bp, BoxLayout.X_AXIS));
         ai = new JCheckBox("AI");
-        // ai.doClick();
+        ai.doClick();
         message = new JTextField();
-        // message.addActionListener(this);
+        message.addActionListener(this);
         JButton enter = new JButton("Enter");
-        // enter.addActionListener(this);
+        enter.addActionListener(this);
         bp.add(ai);
         bp.add(message);
         bp.add(enter);
         add(BorderLayout.CENTER, scrollBar);
         add(BorderLayout.SOUTH, bp);
         setVisible(true);
+        sbot = new SimpleBot();
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(message.getText().trim().length() > 0){
+           dialogue.append(message.getText() + "\n ");
+           dialogue.append(TITLE_OF_PROGRAMM.substring(0, 9) +
+                           sbot.sayInReturn(message.getText(), ai.isSelected()) + "\n");
+        }
+        message.setText("");
+        message.requestFocusInWindow();
     }
 }
